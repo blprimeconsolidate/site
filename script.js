b08907805ac4dfb058b840c9ed6c8af55e5cfc61
blprimeconsolidate <blpconsolidate@icloud.com>
2026-02-09 10:29:55 +0000

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const images = document.querySelectorAll('.tattoo-img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.lightbox-close');
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightbox.style.display = 'flex';
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
